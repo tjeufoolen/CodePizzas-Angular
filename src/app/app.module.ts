436a54415b9331c74f4e8ed5f0fc1ff5b5f185b4
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2023-01-06 13:42:01 +0100

    Initial structure commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsListComponent } from './features/products/components/products-list/products-list.component';
+import { ListComponent } from './features/products/components/list/list.component';
+import { DetailsComponent } from './features/products/components/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent,
+    ListComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
